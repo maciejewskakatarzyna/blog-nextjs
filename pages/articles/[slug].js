91abfd50f718439f3179ad4411e11e4194db9204
kasiamaciejewska <maciejewska.kontakt@gmail.com>
2022-09-23 13:47:32 +0200

    add articles layout and slug

---
+++ pages/articles/[slug].js
@@ -0,0 +1,37 @@
+import Layout from 'components/Layout';
+import Head from 'next/head';
+import { getArticle, getListOfArticles } from 'services/articles';
+
+export const getStaticPaths = async () => {
+  const articles = getListOfArticles('_articles');
+
+  return {
+    paths: articles.map((art) => ({ params: { slug: art.slug } })),
+    fallback: false
+  };
+};
+
+export const getStaticProps = async (req) => {
+  const { slug } = req.params;
+  const article = await getArticle(slug);
+  return {
+    props: { article }
+  };
+};
+
+export default function Article({ article }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{article.title}</title>
+      </Head>
+      <div>
+        <h1 className="text-center text-3xl mb-10">{article.title}</h1>
+        <div
+          className="max-w-3xl mx-auto articleBody"
+          dangerouslySetInnerHTML={{ __html: article.content }}
+        />
+      </div>
+    </Layout>
+  );
+}
